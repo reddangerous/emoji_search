dd78ccbbf208fdcc985a45a64941bdee7d6bf36b
reddangerous <dkinyanjui144@gmail.com>
2023-06-01 14:42:42 +0300

    First Commit

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <h1>Emoji Picker</h1>
+        <p>This emoji Picker is made Possible by React Emoji Picker</p>
+       <p className="footer">Made with <span role="img" aria-label="love">❤️</span> by <a href="https://davidwainaina.netlify.app/">David .K. Wainaina</a></p>
+        </footer>
+
+  )
+}
+
+export default Footer
---
+++ src/MyEmojiPicker.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import EmojiPicker from 'emoji-picker-react';
+
+const MyEmojiPicker = ({ onEmojiClick , EmojiStyle}) => {
+  const [emoji, setEmoji] = useState(null);
+  const [copied, setCopied] = useState(false);
+  const [showPicker, setShowPicker] = useState(false);
+  
+
+  const handleEmojiClick = (emoji) => {
+    setEmoji(emoji);
+    onEmojiClick &&onEmojiClick(emoji);
+    setCopied(false);
+  };
+
+  const handleCopyEmoji = () => {
+    if (emoji) {
+      navigator.clipboard.writeText(emoji.emoji);
+      setCopied(true);
+    }
+  };
+
+  const handleShowPicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+ 
+
+  return (
+    <div className="emoji-picker">
+      <button onClick={handleShowPicker} className='button'>
+        {showPicker ? 'Hide Emojis' : 'Show Emojis'}
+      </button>
+      <div className="selected-emoji">
+        {emoji ? (
+          <span onClick={handleCopyEmoji} className='emoji'>{emoji.emoji} {EmojiStyle}</span>
+        ) : (
+          <span>No emoji chosen</span>
+        )}
+      </div>
+      {copied && <p>Emoji copied!</p>}
+
+      {showPicker && (
+        <div className='emojiPicker'>
+          <EmojiPicker
+            size='2em'
+            onEmojiClick={handleEmojiClick}
+            disableAutoFocus={true}
+          />
+        </div>
+      )}
+
+      
+    </div>
+  );
+};
+
+export default MyEmojiPicker;
